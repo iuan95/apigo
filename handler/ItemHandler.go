abb5e6a1c4b61146590c31f70e3673943dbf6907
iuan <ayaanalekseev@mail.ru>
2024-03-20 20:56:16 +0900

    save

--- handler/ItemHandler.go
+++ handler/ItemHandler.go
@@ -13,7 +13,7 @@ import (
 
 func GetItems(c *fiber.Ctx) error{
 	var items []model.Items
-	conn, err:=db.DB.Query(context.Background(), "SELECT * FROM items")
+	conn, err:=db.DB.Query(context.Background(), "SELECT * FROM items ORDER BY id")
 	if err != nil{
 		return c.Status(403).JSON(&fiber.Map{
 			"message": "error can not get items",
